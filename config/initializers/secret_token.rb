53fa78e5a3930606a4e45144556df713ccd225af
Ig@reK <kigorek@mail.ru>
2015-08-07 12:38:05 +0300

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Matarikari::Application.config.secret_key_base = 'e40d751d2631ec0108d3454a00135ff9fe833dd07a1af39540b3ab7b2bef462cef3df1d29ba817703c218c71b662c0d3238a125a81d468e4331781a038db170a'
