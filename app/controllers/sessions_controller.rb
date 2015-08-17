3723b051d8fff7037c3b4284fae22073175c0adf
Ig@reK <kigorek@mail.ru>
2015-08-17 09:43:34 +0300

    user end

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,18 +2,16 @@ class SessionsController < ApplicationController
 	def new
   	end
 
-	def create
-    	user = User.find_by(email: params[:session][:email].downcase)
-    	if user && user.authenticate(params[:session][:password])
- 			sign_in user
-    	 	redirect_to user
-
-        else
-    		flash.now[:error] = 'Неверное имя пользователя или пароль' 
-      		render 'new'
-      	end
-
+def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_back_or user
+    else
+      flash.now[:error] = 'Неверный email или пароль'
+      render 'new'
     end
+  end
   
 
 
